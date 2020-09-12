1831b9008431f3e1292c64dd8ea95c7215f51632
LukyanovSemyon <lukyakha@gmail.com>
2020-09-12 20:56:16 +0300

    6. Date. Отображение даты.[310017#360546]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item time = new Item();
+        LocalDateTime x = time.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = x.format(formatter);
+        System.out.println("Текущие дата и время: " + currentDateTimeFormat);
+    }
+}

ef0bc1787ccaa2b7d2f4e74b9086929f2c627c2b
LukyanovSemyon <lukyakha@gmail.com>
2020-09-28 18:03:22 +0300

    2. Singleton.[94606#360521]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.IsNull.nullValue;
 
@@ -38,5 +39,33 @@ public class TrackerTest {
         int id = bug.getId();
         tracker.delete(id);
         assertThat(tracker.findById(id), is(nullValue()));
+    }
+
+    @Test
+    public void s1() {
+        TrackerSingle1 t = TrackerSingle1.INSTANCE;
+        TrackerSingle1 t2 = TrackerSingle1.INSTANCE;
+        assertEquals(t, t2);
+    }
+
+    @Test
+    public void s2() {
+        TrackerSingle2 t = TrackerSingle2.getInstance();
+        TrackerSingle2 t2 = TrackerSingle2.getInstance();
+        assertEquals(t, t2);
+    }
+
+    @Test
+    public void s3() {
+        TrackerSingle3 t = TrackerSingle3.getInstance();
+        TrackerSingle3 t2 = TrackerSingle3.getInstance();
+        assertEquals(t, t2);
+    }
+
+    @Test
+    public void s4() {
+        TrackerSingle4 t = TrackerSingle4.getInstance();
+        TrackerSingle4 t2 = TrackerSingle4.getInstance();
+        assertEquals(t, t2);
     }
 }

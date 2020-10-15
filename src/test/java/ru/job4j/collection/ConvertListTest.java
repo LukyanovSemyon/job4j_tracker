93849f510268c61795d7831230e386d4489cc394
LukyanovSemyon <lukyakha@gmail.com>
2020-10-15 05:07:24 +0300

    1. Метод .of[70623#360311]

--- src/test/java/ru/job4j/collection/ConvertListTest.java
+++ src/test/java/ru/job4j/collection/ConvertListTest.java
@@ -2,7 +2,6 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,9 +11,10 @@ import static org.junit.Assert.*;
 public class ConvertListTest {
     @Test
     public void whenTwoList() {
-        List<int[]> in = new ArrayList<>();
-        in.add(new int[] {1});
-        in.add(new int[] {2, 3});
+        List<int[]> in = List.of(
+        new int[] {1},
+        new int[] {2, 3}
+        );
         List<Integer> expect = Arrays.asList(1, 2, 3);
         assertThat(ConvertList.convert(in), is(expect));
     }
--- src/test/java/ru/job4j/lambda/FunctionRangeTest.java
+++ src/test/java/ru/job4j/lambda/FunctionRangeTest.java
@@ -2,7 +2,6 @@ package ru.job4j.lambda;
 
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -12,21 +11,21 @@ public class FunctionRangeTest {
     @Test
     public void whenLinearFunctionThenLinearResults() {
         List<Double> result = FunctionRange.diapason(5, 8, (x) -> 2 * x + 1);
-        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        List<Double> expected = List.of(11D, 13D, 15D);
         assertThat(result, is(expected));
     }
 
     @Test
     public void whenQuadraticFunction() {
         List<Double> result = FunctionRange.diapason(5, 8, (x) -> 3 * x * x + 4 * x + 5);
-        List<Double> expected = Arrays.asList(100D, 137D, 180D);
+        List<Double> expected = List.of(100D, 137D, 180D);
         assertThat(result, is(expected));
     }
 
     @Test
     public void whenExponentialFunction() {
         List<Double> result = FunctionRange.diapason(5, 8, (x) -> Math.pow(3, x));
-        List<Double> expected = Arrays.asList(243D, 729D, 2187D);
+        List<Double> expected = List.of(243D, 729D, 2187D);
         assertThat(result, is(expected));
     }
 }
